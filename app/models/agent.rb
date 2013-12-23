6c0d6d4abdbb7c29dc58458560cbc1a05d02e747
Victor Ivanov <vic.ivanoff@gmail.com>
2013-12-24 00:09:22 +0800

    clear state code from the phone/fax number - 3

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -12,7 +12,7 @@ class Agent < ActiveRecord::Base
   def clear_phone_number phone_number
     return nil unless phone_number
     matches = phone_number.to_s.gsub(/\s/, '').match(/\d\d(\d{8,8})/)
-    return phone_number if !mathes || matches.count == 0
+    return phone_number if !matches || matches.count == 0
     local_number = matches.first
     %w(or fax).each do |search_string|
       local_number = "#{search_string} #{local_number}" if phone_number.include? search_string
