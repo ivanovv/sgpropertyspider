048a7dadf92e598b305fb7b9f1e1aa8ee75ec11c
Victor Ivanov <vic.ivanoff@gmail.com>
2013-12-23 19:09:49 +0800

    clear state code from the phone/fax number

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -9,10 +9,31 @@ class Agent < ActiveRecord::Base
     )
   end
 
+  def clear_phone_number phone_number
+    return nil unless phone_number
+    phone_number = phone_number.to_s
+    local_number = phone_number.gsub(/\s/, '').match(/\d\d(\d{8,8})/)[1]
+    %w(or fax).each do |search_string|
+      local_number = "#{search_string} #{local_number}" if phone_number.include? search_string
+    end
+    local_number
+  end
+
   def to_csv_row
+
+    processed_numbers = []
+    [phone, fax].each_with_index do |number, index|
+      begin
+        processed_numbers[index] = clear_phone_number number
+      rescue
+        puts number
+        raise
+      end
+    end
+
     CSV::Row.new(
         [:name, :company, :position, :phone, :fax, :email, :cea_lic],
-        [name, company, position, phone, fax, email, "#{cea_license_number}/#{cea_reg_number}"]
+        [name, company, position, processed_numbers[0] || phone, processed_numbers[1] || fax, email, "#{cea_license_number}/#{cea_reg_number}"]
     )
   end
 
