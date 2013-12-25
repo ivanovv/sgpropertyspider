0f26a9c3ab35f85140c5c7677dcf856b0f8c4545
Victor Ivanov <vic.ivanoff@gmail.com>
2013-12-25 15:40:44 +0700

    only 65 is removed from phone numbers

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -11,7 +11,7 @@ class Agent < ActiveRecord::Base
 
   def clear_phone_number phone_number
     return nil unless phone_number
-    matches = phone_number.to_s.gsub(/\s/, '').match(/\d\d(\d{8,8})/)
+    matches = phone_number.to_s.gsub(/\s/, '').match(/65(\d{8,8})/)
     return phone_number if !matches
     local_number = matches[1]
     %w(or fax).each do |search_string|
